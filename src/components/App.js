22ad3b887c0c9fc7dad8029ac2495689d67a932d
Jordan Jennings <jordanjennings12@gmail.com>
2022-09-06 13:04:54 -0400

    commit tree a9acbb9d69b54832ec219ba994dc856ae16ff415

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
 // create your App component here
+import { useState, useEffect } from "react";
+
+export default function App () {
+    const [img, setImg] = useState("")
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then(data => setImg(data.message))
+    }, [])
+
+    if(!img) return <p>Loading...</p>
+
+    return (
+        <div>
+            <img  src={img} alt="A Random Dog"/>
+        </div>
+    )
+}
+
